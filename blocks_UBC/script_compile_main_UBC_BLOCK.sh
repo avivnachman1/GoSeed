4fea31501972d28bc2cb5d39518238e6e128f6ce
avivnachman1 <aviv.nachman@campus.technion.ac.il>
2019-12-13 18:44:28 +0200

    changed file names, didn't check yet

--- blocks_UBC/script_compile_main_UBC_BLOCK.sh
+++ blocks_UBC/script_compile_main_UBC_BLOCK.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++11 -O3 -Wall main_UBC_BLOCK.cpp -o main_UBC_BLOCK -I/opt/gurobi811/linux64/include -L/opt/gurobi811/linux64/lib -lgurobi_c++ -lgurobi81
+g++ -std=c++11 -O3 -Wall GoSeedBlocks.cpp -o GoSeedBlocks -I/opt/gurobi811/linux64/include -L/opt/gurobi811/linux64/lib -lgurobi_c++ -lgurobi81
--- containers_UBC/script_compile_main_UBC_CONT.sh
+++ containers_UBC/script_compile_main_UBC_CONT.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -std=c++11 -Wall main_UBC_CONT.cpp -o main_UBC_CONT -I/opt/gurobi811/linux64/include -L/opt/gurobi811/linux64/lib -lgurobi_c++ -lgurobi81
+g++ -std=c++11 -Wall GoSeedCont.cpp -o GoSeedCont -I/opt/gurobi811/linux64/include -L/opt/gurobi811/linux64/lib -lgurobi_c++ -lgurobi81
